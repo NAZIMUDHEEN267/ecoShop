a9d496a26015356aa574bb3fb8d9492138ccafe8
nazimudheen267 <nazimudheen267@gmail.com>
2023-03-04 03:26:54 +0530

    rne added

--- src/navigations/index.jsx
+++ src/navigations/index.jsx
@@ -1,10 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import IntroNavigation from "./IntroNavigation";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function MainNavigator () {
     return (
         <NavigationContainer>
-            <IntroNavigation />
+            <SafeAreaProvider>
+                <IntroNavigation />
+            </SafeAreaProvider>
         </NavigationContainer>
     )
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,35 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
+import { Header, HeaderProps, Input } from "@rneui/themed";
+import { colors } from '../constants';
+import tw from "twrnc";
 
 export class Home extends Component {
   render() {
     return (
-      <View>
-        <Text>Home</Text>
-      </View>
+     <View>
+      <Header 
+        containerStyle={tw`h-3/6 flex-col`}
+        leftContainerStyle={tw`flex-col bg-yellow-200 flex-1`}
+        backgroundColor={colors.PRIMARY_COLOR}
+        barStyle={'light-content'}
+        leftComponent={() => (
+          <View>
+            <Text>hello</Text>
+          </View>
+        )}
+        rightComponent={() => (
+          <View>
+            <Text>hello</Text>
+          </View>
+        )}
+        centerComponent={() => (
+          <View>
+            <Text>hello</Text>
+          </View>
+        )}
+      />
+     </View>
     )
   }
 }
