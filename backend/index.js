a5659323aded0c0fc93ee7413762e01111c5654c
nazimudheen267 <nazimudheen267@gmail.com>
2023-03-18 23:56:55 +0530

    stripe server added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const Stripe = require("stripe");
+require('dotenv').config();
 
-console.log(Stripe);
 const app = express();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+app.use(express.json());
+
+app.post("/create-payment-intent", async (req, res) => {
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 5000,
+        currency: "usd"
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    })
+})
+
+app.listen(3000, () => {
+    console.log("backend running");
+})
